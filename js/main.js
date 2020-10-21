2c834901be918b34a202669aa2d1e48c7a7985c1
unknown <yaramodesu@gmail.com>
2020-10-21 17:36:48 +0300

    vaatteet tehty ja contentful lisätty

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "sqee0b6lmkhy",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "SqoNU9BCfbylzjSw6yLe1iNqYZAoHMyQKOkQJxCejic"
+  });
+
+  
+   client.getEntries()
+.then((response) => console.log(response.items))
+.catch(console.error)
 
 // avaa lähetä viesti ikkuna
 $('.tilaukset .btn1').on('click', function() {
