437442f81f8f67a8314e1e19c9a7de341b7fdaf2
unknown <yaramodesu@gmail.com>
2020-11-06 14:56:26 +0200

    contentful imaget lisätty indexiin

--- js/main.js
+++ js/main.js
@@ -5,9 +5,26 @@ const client = contentful.createClient({
     accessToken: "SqoNU9BCfbylzjSw6yLe1iNqYZAoHMyQKOkQJxCejic"
   });
 
-  client.getEntries()
-.then((response) => console.log(response.items))
-.catch(console.error)
+let tuote1 = document.querySelector('.tuote1');
+let tuote2 = document.querySelector('.tuote2');
+
+  client.getEntries().then ( e =>{
+    const article1 = e.items[3].fields.nimi
+    const article2 = e.items[5].fields.nimi
+    const hinta1 = e.items[3].fields.hinta
+    const hinta2 = e.items[5].fields.hinta 
+    const image1 = e.items[3].fields.image.fields.file.url;
+    const image2 = e.items[5].fields.image.fields.file.url;
+    tuote1.innerHTML = ` <h4>${article1}</h4>
+                    <img src="${image1}">
+                    <h5>€${hinta1}</h5>`
+    tuote2.innerHTML = ` <h4>${article2}</h4>
+                    <img src="${image2}">
+                    <h5>€${hinta2}</h5>`
+                    console.log(hinta1)
+})
+
+
 // avaa lähetä viesti ikkuna
 $('.tilaukset .btn1').on('click', function() {
     $('.form1').toggleClass('hidden')
